25ef4a011b0b35959f70c0e37465f36c7edfd2f8
Sonkeng Maldini <sdmg15@pm.me>
2022-02-10 08:47:00 +0100

    refact tests and add missing one

--- test/registry-test.js
+++ test/registry-test.js
@@ -63,6 +63,23 @@ describe("BlockHeaderRegistry", async () => {
     };
 
   });
+
+  const addSigBlocks = async () => {
+    const rlpHeader = ethers.utils.RLP.encode(
+      Object.values(header).map((v) => (v === 0 ? "0x" : v))
+    );
+    const payload = ethers.utils.keccak256(rlpHeader);
+    const { _vs: vs, r } = ethers.utils.splitSignature(
+      await signer.signMessage(ethers.utils.arrayify(payload))
+    );
+    const tx = await blockHeaderRegistry
+      .connect(signer)
+      .addSignedBlocks([[rlpHeader, [r, vs], 1, payload, 0, []]]);
+    const rx = await tx.wait();
+    expect(rx.status).to.equal(1);
+    return payload;
+  };
+
   describe("Blockchains", () => {
     it("Should add a new blockchain", async () => {
       const tx = await voting.addBlockchain(
@@ -82,7 +99,9 @@ describe("BlockHeaderRegistry", async () => {
       ).to.be.revertedWith("onlyVoting");
     });
   });
+
   describe("Signed blocks", () => {
+
     it("Should fail if the rlpHeaderHash is not the blockHash", async () => {
 
       const rlpHeader = ethers.utils.RLP.encode(
@@ -100,37 +119,15 @@ describe("BlockHeaderRegistry", async () => {
           ])
       ).to.be.revertedWith("rlpHeaderHash");
     });
+
     it("Should fail if there are no signed blocks at some height", async () => {
       await expect(blockHeaderRegistry.getSignedBlock(1, 0)).to.be.revertedWith(
         "_blockHashes.length"
       );
     });
+
     it("Should not add a signed block at the same height", async () => {
       const signer = signers[0];
-      const header = {
-        ParentHash:
-          "0x1e77d8f1267348b516ebc4f4da1e2aa59f85f0cbd853949500ffac8bfc38ba14",
-        UncleHash:
-          "0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347",
-        Coinbase: "0x2a65Aca4D5fC5B5C859090a6c34d164135398226",
-        Root: "0x0b5e4386680f43c224c5c037efc0b645c8e1c3f6b30da0eec07272b4e6f8cd89",
-        TxHash:
-          "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
-        ReceiptHash:
-          "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
-        Bloom:
-          "0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
-        Difficulty: ethers.utils.hexlify(6022643743806),
-        Number: ethers.utils.hexlify(400000),
-        GasLimit: ethers.utils.hexlify(3141592),
-        GasUsed: "0x", //(0).toString(16),
-        Time: ethers.utils.hexlify(1445130204),
-        Extra: "0xd583010202844765746885676f312e35856c696e7578",
-        MixDigest:
-          "0x3fbea7af642a4e20cd93a945a1f5e23bd72fc5261153e09102cf718980aeff38",
-        Nonce: "0x6af23caae95692ef",
-        //				'BaseFee': 0
-      };
       const rlpHeader = ethers.utils.RLP.encode(
         Object.values(header).map((v) => (v === 0 ? "0x" : v))
       );
@@ -148,6 +145,7 @@ describe("BlockHeaderRegistry", async () => {
           .addSignedBlocks([[rlpHeader, [r, vs], 1, payload, 0, []]])
       ).to.be.revertedWith("hasSigned");
     });
+
     it("Should not add a signed block from another signer", async () => {
       const rlpHeader = ethers.utils.RLP.encode(
         Object.values(header).map((v) => (v === 0 ? "0x" : v))
@@ -162,32 +160,26 @@ describe("BlockHeaderRegistry", async () => {
           .addSignedBlocks([[rlpHeader, [r, vs], 1, payload, 0, []]])
       ).to.be.revertedWith("msg.sender == signer");
     });
+    
     it("Should add and get a EVM signed block", async () => {
-      const rlpHeader = ethers.utils.RLP.encode(
-        Object.values(header).map((v) => (v === 0 ? "0x" : v))
-      );
-      const payload = ethers.utils.keccak256(rlpHeader);
-      const { _vs: vs, r } = ethers.utils.splitSignature(
-        await signer.signMessage(ethers.utils.arrayify(payload))
-      );
-      const tx = await blockHeaderRegistry
-        .connect(signer)
-        .addSignedBlocks([[rlpHeader, [r, vs], 1, payload, 0, []]]);
-      const rx = await tx.wait();
-      expect(rx.status).to.equal(1);
+      const payload = await addSigBlocks();
       const bhash = await blockHeaderRegistry.blockHashes(1, header.Number, 0);
       expect(bhash).to.equal(payload);
       const block = await blockHeaderRegistry.getSignedBlock(1, header.Number);
       expect(block.signedBlock.creator).to.equal(signer.address);
     });
-    it("Should return the block hash with most signatures", async () => {});
+
+    it("Should return the block hash with most signatures", async () => {
+      await addSigBlocks();
+      const block = await blockHeaderRegistry.getSignedBlock(1, header.Number);
+      expect(block.blockHash).to.equal("0x5d15649e25d8f3e2c0374946078539d200710afc977cdfc6a977bd23f20fa8e8");
+    });
+
     it("Should not let a non-validator add a signed block", async () => {
       signer = signers[1];
       const rlpHeader = ethers.utils.RLP.encode(
         Object.values(header).map((v) => (v === 0 ? "0x" : v))
       );
-      const blockHash =
-        "0x5d15649e25d8f3e2c0374946078539d200710afc977cdfc6a977bd23f20fa8e8";
       const payload = ethers.utils.keccak256(rlpHeader);
       const { _vs: vs, r } = ethers.utils.splitSignature(
         await signer.signMessage(ethers.utils.arrayify(payload))
@@ -198,6 +190,7 @@ describe("BlockHeaderRegistry", async () => {
           .addSignedBlocks([[rlpHeader, [r, vs], 1, payload, 0, []]])
       ).to.be.revertedWith("onlyValidator");
     });
+
     it("Should add and get a fuse signed block", async () => {
       const rlpHeader = ethers.utils.RLP.encode(
         Object.values(header).map((v) => (v === 0 ? "0x" : v))
